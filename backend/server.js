75bedc9a31a6a033baab3b347210feb348d22aea
ajaycodes02 <ajaycodes646@gmail.com>
2025-09-06 19:31:56 +0530

    create a routes and set the api calls in backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const postRoutes = require('./routes/posts');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -13,4 +14,8 @@ mongoose.connect('mongodb://localhost:27017/blog')
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//Use Routes
+
+app.use('/api/posts', postRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));  
